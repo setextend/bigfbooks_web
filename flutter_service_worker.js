4e423db8eab00480978bee8ea05a284ed7106ff8
setextend <setextend@users.noreply.github.com>
2025-09-09 03:16:25 +0000

    deploy: setextend/bigfbooks@8158626f4250796dab8a92eec74d69bab0f8630f

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -31,7 +31,7 @@ const RESOURCES = {"canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
 "assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "33b7d9392238c04c131b6ce224e13711",
-"flutter_bootstrap.js": "b375204deb6193246f60efea0422e77e",
+"flutter_bootstrap.js": "6225d6e45d81bb4bdcc6e8074169e9cb",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "index.html": "180f40f465ff45a926c5afddab4b8d25",
 "/": "180f40f465ff45a926c5afddab4b8d25"};
